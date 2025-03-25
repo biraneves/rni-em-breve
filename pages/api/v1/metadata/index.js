fca2fbd26e8a2e37550ecd43cf0cae5041a86e01
Ubirajara Neves <biraneves@outlook.com>
2025-03-25 18:57:05 +0100

    feat(api): :white_check_mark: add metadata to be returned on get

--- pages/api/v1/metadata/index.js
+++ pages/api/v1/metadata/index.js
@@ -1,5 +1,13 @@
 const metadata = async (request, response) => {
-  await response.status(200).json({});
+  const language = 'pt-BR';
+  const title = 'Residência na Itália';
+  const description = 'Realize o Sonho da Cidadania Italiana com Autonomia e Suporte Especializado';
+
+  await response.status(200).json({
+    language,
+    title,
+    description,
+  });
 };
 
 export default metadata;
