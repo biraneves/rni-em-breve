9734d479d1cd7b348393c85ff0c1adeae570e9a1
Ubirajara Neves <biraneves@outlook.com>
2025-03-27 17:37:12 +0100

    fix: :bug: replace all `Link` components with `<a>` tags

--- components/Navbar/NavbarItem/index.js
+++ components/Navbar/NavbarItem/index.js
@@ -1,12 +1,11 @@
-import Link from "next/link";
 import styles from "./navbaritem.module.scss";
 
 const NavbarItem = ({ title, icon, url }) => (
   <li className={styles.item}>
-    <Link href={url} className={styles.item__link}>
+    <a href={url} className={styles.item__link}>
       <span className={`fa-solid fa-${icon}`} />
       <span>{title}</span>
-    </Link>
+    </a>
   </li>
 );
 
