854557cd92ad0df839c196b2286788b61b627518
Ubirajara Neves <biraneves@outlook.com>
2025-03-27 14:06:02 +0100

    feat(css): :lipstick: add global styles do the home page

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,23 @@ import GeneralHead from "../components/GeneralHead";
 import Navbar from "../components/Navbar";
 import siteSections from "../data/siteSections";
 import NavbarItem from "../components/Navbar/NavbarItem";
+import styles from './home.module.scss';
 
 const Home = () => {
   return (
     <>
       <GeneralHead />
-      <header>
-        <Link href="/">
-          <img src="/img/rni-logo.png" alt="Residência na Itália" />
-        </Link>
-        <Navbar>
-          {siteSections.map((item, index) => (
-            <NavbarItem key={index} title={item.title} icon={item.icon} url={item.url} />
-          ))}
-        </Navbar>
+      <header className={styles.header}>
+        <div className={styles.container}>
+          <Link href="/">
+            <img src="/img/rni-logo.png" alt="Residência na Itália" />
+          </Link>
+          <Navbar>
+            {siteSections.map((item, index) => (
+              <NavbarItem key={index} title={item.title} icon={item.icon} url={item.url} />
+            ))}
+          </Navbar>
+        </div>
       </header>
       <main>
       </main>
