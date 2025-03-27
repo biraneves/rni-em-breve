2dda3bbda2ad33e8d2f22db924aa45ca10819d4a
Ubirajara Neves <biraneves@outlook.com>
2025-03-27 11:44:49 +0100

    feat: :sparkles: add array with site sections and its characteristics

---
+++ data/siteSections.js
@@ -0,0 +1,39 @@
+const siteSections = [
+  {
+    title: 'Início',
+    url: '#banner',
+    icon: 'house',
+  },
+  {
+    title: 'Quem somos',
+    url: '#about-us',
+    icon: 'people-group',
+  },
+  {
+    title: 'Serviços',
+    url: '#services',
+    icon: 'list-check',
+  },
+  {
+    title: 'Molise',
+    url: '#molise',
+    icon: 'location-dot',
+  },
+  {
+    title: 'Acomodações',
+    url: '#accomodation',
+    icon: 'people-roof',
+  },
+  {
+    title: 'Depoimentos',
+    url: '#testimonials',
+    icon: 'message',
+  },
+  {
+    title: 'Contato',
+    url: '#contact',
+    icon: 'envelope-open',
+  },
+];
+
+export default siteSections;
