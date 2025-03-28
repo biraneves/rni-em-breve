2b4aaec566ffb2decd55b2bad1d3862504b0866a
Ubirajara Neves <biraneves@outlook.com>
2025-03-28 09:17:43 +0100

    feat: :sparkles: add array of accommodations

---
+++ data/accommodations.js
@@ -0,0 +1,16 @@
+const accommodations = [
+  {
+    title: "Casa 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident rerum molestias distinctio suscipit.",
+    image: "https://dummyimage.com/640x480",
+  },
+  {
+    title: "Casa 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident rerum molestias distinctio suscipit.",
+    image: "https://dummyimage.com/640x480",
+  },
+];
+
+export default accommodations;
