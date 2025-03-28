cc4b7171d24791c85fddb2374a06605308a4b1b0
Ubirajara Neves <biraneves@outlook.com>
2025-03-28 10:47:38 +0100

    feat: :sparkles: add array of testimonials

---
+++ data/testimonials.js
@@ -0,0 +1,34 @@
+const testimonials = [
+  {
+    testimonial:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    author: {
+      name: "John Doe",
+      origin: "São Paulo, SP",
+      evaluation: 5,
+      avatar: "https://dummyimage.com/64x64",
+    },
+  },
+  {
+    testimonial:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    author: {
+      name: "John Doe",
+      origin: "São Paulo, SP",
+      evaluation: 5,
+      avatar: "https://dummyimage.com/64x64",
+    },
+  },
+  {
+    testimonial:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    author: {
+      name: "John Doe",
+      origin: "São Paulo, SP",
+      evaluation: 5,
+      avatar: "https://dummyimage.com/64x64",
+    },
+  },
+];
+
+export default testimonials;
